b55983432457bdb25872b45f317dbe864254c7fe
Joris Aerts <3611758+JorisAerts@users.noreply.github.com>
2024-08-07 21:39:44 +0200

    [node] temp-dir

--- src/node/utils/temp-dir.ts
+++ src/node/utils/temp-dir.ts
@@ -1,17 +1,12 @@
-import { mkdtempSync } from 'fs'
+import { existsSync, mkdirSync } from 'fs'
 import { join } from 'path'
 import os from 'os'
 import { packageJson } from './package'
 
-let tmp: string
-
-let t = () => {
-  tmp = mkdtempSync(join(os.tmpdir(), `${packageJson.name!}-`))
-  t = () => tmp
-  return tmp
-}
+const tmp = join(os.tmpdir(), `${packageJson.name!}`)
+if (!existsSync(tmp)) mkdirSync(tmp, { recursive: true })
 
 /**
  * a getter function, which will create the temp-folder upon first access
  */
-export const tempDir = () => t()
+export const tempDir = () => tmp
