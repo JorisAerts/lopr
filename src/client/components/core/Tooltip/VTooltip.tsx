d98c5d3db3d4a02f303b0e94dabbcacb481fa8d3
Joris Aerts <3611758+JorisAerts@users.noreply.github.com>
2024-08-08 22:45:18 +0200

    [client] Tooltip position

--- src/client/components/core/Tooltip/VTooltip.tsx
+++ src/client/components/core/Tooltip/VTooltip.tsx
@@ -26,16 +26,17 @@ export const VTooltip = defineComponent({
       const pos = {
         x: root.value?.offsetLeft ?? 0,
         y: root.value?.offsetTop ?? 0,
-        h: (root.value?.offsetHeight ?? 0) + props.margin,
+        h: root.value?.offsetHeight ?? 0,
         w: root.value?.offsetWidth ?? 0,
       }
       const tooltip = {
-        h: (dlg.value?.offsetHeight ?? 0) + props.margin,
-        w: (dlg.value?.offsetWidth ?? 0) + props.margin,
+        h: dlg.value?.offsetHeight ?? 0,
+        w: dlg.value?.offsetWidth ?? 0,
       }
+      const above = pos.y - tooltip.h - props.margin
       return {
-        top: pos.y - tooltip.h < 0 ? `${pos.y + pos.h}px` : `${pos.y - tooltip.h}px`,
-        left: pos.x + tooltip.w > document.body.clientWidth ? `${pos.x - (tooltip.w - props.margin) + pos.w}px` : `${pos.x}px`,
+        top: `${tooltip.h === 0 ? 0 : above < 0 ? pos.y + pos.h + props.margin : above}px`,
+        left: `${tooltip.w === 0 ? 0 : pos.x + tooltip.w + props.margin > document.body.clientWidth ? pos.x + pos.w - tooltip.w : pos.x}px`,
       }
     })
     const tooltip = computed(() => slots.tooltip?.() ?? props.text)
