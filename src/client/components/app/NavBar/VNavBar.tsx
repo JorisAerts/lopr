bbbfc19de8d42999d3f0554126f6070ee334228d
Joris Aerts <3611758+JorisAerts@users.noreply.github.com>
2024-08-08 11:26:01 +0200

    [client] Navigation: error log

--- src/client/components/app/NavBar/VNavBar.tsx
+++ src/client/components/app/NavBar/VNavBar.tsx
@@ -1,19 +1,22 @@
 import { defineComponent } from 'vue'
-import { VCard, VIcon } from '../../core'
+import { VBtn, VCard, VIcon, VSpacer, VTooltip } from '../../core'
 import { APP_NAME } from '../../../../shared/constants'
 import './VNavBar.scss'
 
 export const VNavBar = defineComponent({
   name: 'v-nav-bar',
 
-  setup(props, { slots }) {
+  setup() {
     return () => (
-      <VCard class={['v-nav-bar', 'pa-2', 'pl-3', 'd-flex', 'gap-2']}>
-        <h4 class={['d-flex']}>
+      <VCard class={['v-nav-bar', 'pa-2', 'd-flex', 'gap-2', 'align-items-center']}>
+        <h4 class={['d-flex', 'align-center']}>
           <VIcon name={'DeployedCode_Fill'} size={22} class={['mr-1']} />
           {APP_NAME}
         </h4>
-        {slots.default?.()}
+        <VSpacer />
+        <VTooltip text={'Error log'}>
+          <VBtn icon={'Monitoring'} size={20} class={['pa-1']} transparent />
+        </VTooltip>
       </VCard>
     )
   },
