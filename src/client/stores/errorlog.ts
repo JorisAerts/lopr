5b1f5c3aa45245fec3254d4a0f0696f78061d9d1
Joris Aerts <3611758+JorisAerts@users.noreply.github.com>
2024-08-09 10:50:35 +0200

    [client] Error log

--- src/client/stores/errorlog.ts
+++ src/client/stores/errorlog.ts
@@ -1,20 +1,23 @@
 import { defineStore } from 'pinia'
 import { computed, ref } from 'vue'
-import type { ProxyRequestInfo } from '../../shared/Request'
 import { registerDataHandler } from '../utils/websocket'
 import type { WebSocketMessage } from '../../shared/WebSocketMessage'
 import { WebSocketMessageType } from '../../shared/WebSocketMessage'
 
 export const STORE_NAME = 'Error Log'
 
+type ErrorLog = Record<string, string> & {
+  ts: Date
+}
+
 export const useErrorLogStore = defineStore(STORE_NAME, () => {
   /**
    * A bunch of errors coming from the client
    */
-  const errors = ref([] as unknown[])
+  const errors = ref([] as ErrorLog[])
 
   // register the handlers (they will overwrite the previous ones)
-  registerDataHandler(WebSocketMessageType.Error, ({ data }: WebSocketMessage<ProxyRequestInfo>) => {
+  registerDataHandler(WebSocketMessageType.Error, ({ data }: WebSocketMessage<ErrorLog>) => {
     data.ts = new Date(data.ts)
     errors.value.push(data)
   })
--- src/client/views/ErrorLog.tsx
+++ src/client/views/ErrorLog.tsx
@@ -1,14 +1,25 @@
 import { defineComponent } from 'vue'
 import { VCard, VSheet } from '../components'
+import { useErrorLogStore } from '../stores/errorlog'
 
 export const Sequence = defineComponent({
   name: 'error-log',
 
   setup() {
+    const errorLogStore = useErrorLogStore()
     return () => (
       <VSheet class={['fill-height', 'gap-2']}>
         <VCard class={['fill-height', 'overflow-auto', 'flex-grow-1', 'pa-3']}>
           <h2>Error log</h2>
+          <VSheet class={['d-flex', 'flex-column', 'gap-2']}>
+            {errorLogStore.errors.map((err) => (
+              <VCard>
+                {err.ts && <div>Timestamp: {err.ts.toLocaleString()}</div>}
+                {err.name && <div>name: {err.name}</div>}
+                {err.message && <div>message: {err.message}</div>}
+              </VCard>
+            ))}
+          </VSheet>
         </VCard>
       </VSheet>
     )
