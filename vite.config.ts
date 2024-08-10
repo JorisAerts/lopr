bf04c3b87cb9d1d57699e19688b0e67d43771f57
Joris Aerts <3611758+JorisAerts@users.noreply.github.com>
2024-08-10 11:38:02 +0200

    [client] temporarily disable Vite VueDevTools plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { defineConfig } from 'vite'
-import VueDevTools from 'vite-plugin-vue-devtools'
-import {
-  DEFAULT_HOST,
-  DEFAULT_PORT,
-  WEBSOCKET_ROOT,
-} from './src/shared/constants'
+//import VueDevTools from 'vite-plugin-vue-devtools'
+import { DEFAULT_HOST, DEFAULT_PORT, WEBSOCKET_ROOT } from './src/shared/constants'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [VueDevTools(), vue(), vueJsx()],
+  plugins: [/*VueDevTools(),*/ vue(), vueJsx()],
   base: './',
 
   build: {
