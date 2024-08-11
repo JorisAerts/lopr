4b35fb6fa460452209b095bb26a612dea20ce5f3
Joris Aerts <3611758+JorisAerts@users.noreply.github.com>
2024-08-12 01:31:41 +0200

    [client] App Controls: clear button

--- src/client/components/app/NavBar/AppControlsToolbar.tsx
+++ src/client/components/app/NavBar/AppControlsToolbar.tsx
@@ -3,16 +3,18 @@ import { defineComponent } from 'vue'
 import { VBtn, VSheet } from '../../core'
 import { PlayPauseButton } from '../PlayPauseButton'
 import { useAppStore } from '../../../stores/app'
+import { useRequestStore } from '../../../stores/request'
 
 export const AppControlsToolbar = defineComponent({
   name: 'AppControlsToolbar',
 
   setup() {
     const appStore = useAppStore()
+    const requestStore = useRequestStore()
     return () => (
       <VSheet class={['v-app-controls-toolbar']}>
         <PlayPauseButton v-model:recording={appStore.recording} />
-        <VBtn class={['align-center', 'pa-1']} icon={'Delete'} size={22} transparent onClick={() => {}} />
+        <VBtn class={['align-center', 'pa-1']} icon={'Delete'} size={22} transparent onClick={requestStore.clear} />
       </VSheet>
     )
   },
