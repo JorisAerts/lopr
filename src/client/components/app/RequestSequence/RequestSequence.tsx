a718b887cc5d6c7656f553203c38731473b8eeb9
Joris Aerts <3611758+JorisAerts@users.noreply.github.com>
2024-08-12 13:18:02 +0200

    [client] request sequence: only scroll when nothing is selected

--- src/client/components/app/RequestSequence/RequestSequence.tsx
+++ src/client/components/app/RequestSequence/RequestSequence.tsx
@@ -22,7 +22,9 @@ export const RequestSequence = defineComponent({
     const handleSelect = (item: ProxyRequestInfo) => {
       emit('update:modelValue', item)
     }
-    watch(requestStore.ids, () => list.value?.$el?.lastElementChild?.scrollIntoView())
+    watch(requestStore.ids, () => {
+      if (!props.modelValue) list.value?.$el?.lastElementChild?.scrollIntoView()
+    })
     return () => (
       <VList class={['fill-height', 'overflow-auto', 'mt-2']} ref={list}>
         <TransitionGroup>
