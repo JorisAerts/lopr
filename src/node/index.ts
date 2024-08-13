596e2aec0acf2c61260b5bddbb3af2ae0e84cceb
Joris Aerts <3611758+JorisAerts@users.noreply.github.com>
2024-08-13 10:57:06 +0200

    [node] longer stacktraces

--- src/node/index.ts
+++ src/node/index.ts
@@ -5,7 +5,13 @@ import { openBrowser } from './utils/open-browser'
 import { displayServerInfo } from './server/server-info'
 import { processCliParams } from './cli'
 
+// allow unsage SSL certificates
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+
+// stacktraces
+if (process.env.NODE_ENV !== 'production') {
+  import('longjohn')
+}
 
 const cliOptions = processCliParams()
 const options: Partial<CreateProxyOptions> = {
