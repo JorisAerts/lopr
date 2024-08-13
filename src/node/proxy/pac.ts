cd3dc84c1d0c728d8ff7a1118d7eaad79ae02172
Joris Aerts <3611758+JorisAerts@users.noreply.github.com>
2024-08-13 20:48:32 +0200

    [node] PAC: if it's not http, then it will never be https

--- src/node/proxy/pac.ts
+++ src/node/proxy/pac.ts
@@ -8,7 +8,6 @@ export const generatePac = (proxy: string) =>
 function FindProxyForURL(url, host) {
   if (
     url.substring(0, 4) === 'http' ||
-    url.substring(0, 5) === 'https' ||
     url.substring(0, 3) === 'ws:'
   ) {
     return 'PROXY ${proxy}'
