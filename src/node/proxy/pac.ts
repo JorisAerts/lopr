8d86b2ebb90bbfa7a9f32692376a61ce2f08cc35
Joris Aerts <3611758+JorisAerts@users.noreply.github.com>
2024-08-13 20:49:37 +0200

    [node] PAC: ...but it might be wss

--- src/node/proxy/pac.ts
+++ src/node/proxy/pac.ts
@@ -8,7 +8,8 @@ export const generatePac = (proxy: string) =>
 function FindProxyForURL(url, host) {
   if (
     url.substring(0, 4) === 'http' ||
-    url.substring(0, 3) === 'ws:'
+    url.substring(0, 3) === 'ws:'  ||
+    url.substring(0, 4) === 'wss:'
   ) {
     return 'PROXY ${proxy}'
   }
