a09f5159bd72c3736c5613be39025e6297bbf7cb
Joris Aerts <3611758+JorisAerts@users.noreply.github.com>
2024-08-15 15:08:58 +0200

    [client] UI tweaking

--- src/client/views/ErrorLog/ErrorLog.tsx
+++ src/client/views/ErrorLog/ErrorLog.tsx
@@ -12,32 +12,36 @@ export const ErrorLog = defineComponent({
         <VCard class={['fill-height', 'overflow-auto', 'flex-grow-1', 'pa-3']}>
           <h2 class={'mb-4'}>Error log</h2>
           <VSheet class={['d-flex', 'flex-column', 'gap-2']}>
-            <TransitionGroup>
-              {errorLogStore.errors.map((err, index) => (
-                <VCard class={['pa-2', 'd-flex']} key={err.key}>
-                  <VBtn class={['align-center', 'pa-1']} icon={'Delete'} size={22} transparent onClick={() => errorLogStore.errors.splice(index, 1)} />
-                  <VSheet class={['pa-2', 'd-flex', 'flex-column', 'flex-grow-0']}>
-                    {err.ts && (
-                      <div>
-                        <label>Timestamp</label>: {err.ts.toLocaleString()}
-                      </div>
-                    )}
-                    {err.src && (
-                      <div>
-                        <label>Source</label>: {typeof err.src === 'string' || typeof err.src === 'number' ? err.src : JSON.stringify(err.src, null, 2)}
-                      </div>
-                    )}
-                    {err.err && (
-                      <pre class={['mt-2', 'text-mono']}>
-                        {Object.keys(err.err).length //
-                          ? JSON.stringify(err.err, null, 2)
-                          : undefined}
-                      </pre>
-                    )}
-                  </VSheet>
-                </VCard>
-              ))}
-            </TransitionGroup>
+            {errorLogStore.errors.length ? (
+              <TransitionGroup>
+                {errorLogStore.errors.map((err, index) => (
+                  <VCard class={['pa-2', 'd-flex']} key={err.key}>
+                    <VBtn class={['align-center', 'pa-1']} icon={'Delete'} size={22} transparent onClick={() => errorLogStore.errors.splice(index, 1)} />
+                    <VSheet class={['pa-2', 'd-flex', 'flex-column', 'flex-grow-0']}>
+                      {err.ts && (
+                        <div>
+                          <label>Timestamp</label>: {err.ts.toLocaleString()}
+                        </div>
+                      )}
+                      {err.src && (
+                        <div>
+                          <label>Source</label>: {typeof err.src === 'string' || typeof err.src === 'number' ? err.src : JSON.stringify(err.src, null, 2)}
+                        </div>
+                      )}
+                      {err.err && (
+                        <pre class={['mt-2', 'text-mono']}>
+                          {Object.keys(err.err).length //
+                            ? JSON.stringify(err.err, null, 2)
+                            : undefined}
+                        </pre>
+                      )}
+                    </VSheet>
+                  </VCard>
+                ))}
+              </TransitionGroup>
+            ) : (
+              'No errors to display'
+            )}
           </VSheet>
         </VCard>
       </VSheet>
--- src/client/views/Information.tsx
+++ src/client/views/Information.tsx
@@ -15,24 +15,25 @@ export const Information = defineComponent({
         <VCard class={['fill-height', 'overflow-auto', 'flex-grow-1', 'pa-3']}>
           <h2>Information</h2>
           Some practical info.
-          <h3 class={['d-flex', 'align-items-center', 'mt-6']}>
-            <VIcon name={'ShieldLock'} size={22} />
-            Certificates
-          </h3>
+          <h3 class={['mt-6']}>Certificates</h3>
           <div class={['d-flex', 'mt-4', 'gap-2', 'flex-wrap']}>
-            <TransitionGroup>
-              {certStore.certificates.map((certFile) => {
-                const file = `file://${certFile.replace(/\\/, '/')}`
-                const cert = file.substring(file.lastIndexOf('/') + 1, file.lastIndexOf('.'))
-                return (
-                  <VCard class={['pa-2']} key={file}>
-                    <a href={file} target={'cert'}>
-                      <VIcon name={'ShieldLock'} /> {cert}
-                    </a>
-                  </VCard>
-                )
-              })}
-            </TransitionGroup>
+            {certStore.certificates.length ? (
+              <TransitionGroup>
+                {certStore.certificates.map((certFile) => {
+                  const file = `file://${certFile.replace(/\\/, '/')}`
+                  const cert = file.substring(file.lastIndexOf('/') + 1, file.lastIndexOf('.'))
+                  return (
+                    <VCard class={['pa-2']} key={file}>
+                      <a href={file} target={'cert'}>
+                        <VIcon name={'ShieldLock'} /> {cert}
+                      </a>
+                    </VCard>
+                  )
+                })}
+              </TransitionGroup>
+            ) : (
+              'No certificate generated yet'
+            )}
           </div>
         </VCard>
       </VSheet>
