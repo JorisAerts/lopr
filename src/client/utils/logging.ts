e4f144f601e9f69a085eef17f940d75c5e63c5e5
Joris Aerts <3611758+JorisAerts@users.noreply.github.com>
2024-08-16 13:29:53 +0200

    [node] error handling

--- src/client/utils/logging.ts
+++ src/client/utils/logging.ts
@@ -4,11 +4,7 @@ import { createErrorMessage } from '../../node/utils/ws-messages'
 
 type LogType = <ErrorType, SourceObject>(error: ErrorType, source?: SourceObject) => unknown
 
-const error: LogType = <ErrorType, SourceObject>(error: ErrorType, source?: SourceObject): void => {
-  // send the error to the websocket
-  sendWsData(WebSocketMessageType.Error, createErrorMessage(error, source))
-}
-
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const warn: LogType = <ErrorType, SourceObject>(message: ErrorType, source?: SourceObject): void => {}
 
 export const createErrorHandler = <ErrorType, Parent>(source?: Parent) =>
@@ -16,11 +12,13 @@ export const createErrorHandler = <ErrorType, Parent>(source?: Parent) =>
     sendWsData(WebSocketMessageType.Error, createErrorMessage(err, source))
   }
 
+/**
+ * Creates error handlers for the given objects.
+ * They will send the error to the WebSocket.
+ *
+ * If no error handler is specified, this would lead to exiting the server process
+ */
 export const createErrorHandlerFor = (...args: { on: (...args: any[]) => unknown }[]) => {
-  args.forEach(
-    (
-      arg //
-    ) => arg.on('error', createErrorHandler(arg))
-  )
+  args?.forEach((arg) => arg.on('error', createErrorHandler(arg)))
   return args?.[0]
 }
