534cbdb426d63b26b4b0fd9dbd7429d71b07a92d
Joris Aerts <3611758+JorisAerts@users.noreply.github.com>
2024-08-21 15:39:41 +0200

    [client] Information page: sort certificates

--- src/client/views/Information.tsx
+++ src/client/views/Information.tsx
@@ -1,15 +1,18 @@
-import { defineComponent, TransitionGroup } from 'vue'
+import { computed, defineComponent, TransitionGroup } from 'vue'
 import { VCard, VIcon, VSheet } from '../components'
 import { useCertificateStore } from '../stores/certificates'
 
 export const Information = defineComponent({
   name: 'app-information',
-
-  props: {},
-
-  setup(props) {
+  setup() {
     const certStore = useCertificateStore()
-
+    const certificates = computed(() =>
+      certStore.certificates?.toSorted().map((certFile) => {
+        const file = `file://${certFile.replace(/\\/, '/')}`
+        const cert = file.substring(file.lastIndexOf('/') + 1, file.lastIndexOf('.'))
+        return { cert, file }
+      })
+    )
     return () => (
       <VSheet class={['fill-height']}>
         <VCard class={['fill-height', 'overflow-auto', 'flex-grow-1', 'pa-3']}>
@@ -19,17 +22,13 @@ export const Information = defineComponent({
           <div class={['d-flex', 'mt-4', 'gap-2', 'flex-wrap']}>
             {certStore.certificates.length ? (
               <TransitionGroup>
-                {certStore.certificates.map((certFile) => {
-                  const file = `file://${certFile.replace(/\\/, '/')}`
-                  const cert = file.substring(file.lastIndexOf('/') + 1, file.lastIndexOf('.'))
-                  return (
-                    <VCard class={['pa-2']} key={file}>
-                      <a href={file} target={'cert'}>
-                        <VIcon name={'ShieldLock'} /> {cert}
-                      </a>
-                    </VCard>
-                  )
-                })}
+                {certificates.value.map(({ file, cert }) => (
+                  <VCard class={['pa-2']} key={file}>
+                    <a href={file} target={'cert'}>
+                      <VIcon name={'ShieldLock'} /> {cert}
+                    </a>
+                  </VCard>
+                ))}
               </TransitionGroup>
             ) : (
               'No certificate generated yet'
