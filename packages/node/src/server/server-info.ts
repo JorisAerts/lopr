87249d247411699cd416d68af3d007eec90fba60
Joris Aerts <3611758+JorisAerts@users.noreply.github.com>
2024-08-23 14:55:30 +0200

    [node] server-info (fix PAC highlighting)

--- packages/node/src/server/server-info.ts
+++ packages/node/src/server/server-info.ts
@@ -24,7 +24,7 @@ export const displayServerInfo = ({ logger, server }: InstanceOptions) => {
   logger.info()
   logger.info(`  ${tui.tooltip(`Temp-folder: ${tui.link(tmp)}`)}`)
   logger.info(`  ${tui.tooltip(`GUI & Proxy Server: ${tui.link(getAddress(server))}`)}`)
-  logger.info(`  ${tui.tooltip(`Automatic proxy configuration (PAC): ${tui.link(getAddress(server))}/pac`)}`)
+  logger.info(`  ${tui.tooltip(`Automatic proxy configuration (PAC): ${tui.link(`${getAddress(server)}/pac`)}`)}`)
   logger.info(`  ${tui.tooltip(`Use ${tui.tip('--open')} to automatically open your browser.`)}`)
   logger.info()
   logger.info(`  ${`Press '${tui.tip('q')}' to quit.`}`)
