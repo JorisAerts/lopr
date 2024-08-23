1f1876325fe3ff5014a0dd2852e5c109ead097d2
Joris Aerts <3611758+JorisAerts@users.noreply.github.com>
2024-08-23 10:48:26 +0200

    [client] monorepo (wip)

---
+++ packages/shared/vite.config.ts
@@ -0,0 +1,31 @@
+import { resolve } from 'path'
+import type { ConfigEnv, LibraryOptions } from 'vite'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig((env: ConfigEnv) => ({
+  esbuild: {
+    minifySyntax: true,
+    minifyIdentifiers: true,
+  },
+  build: {
+    outDir: './dist',
+    ssr: true,
+    sourcemap: env.mode === 'development',
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, './src/index.ts'),
+      name: 'js-proxy-shared',
+      // the proper extensions will be added
+      fileName: 'js-proxy-shared',
+      formats: ['es'],
+    } as LibraryOptions,
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      output: {
+        globals: {},
+      },
+    },
+  },
+}))
