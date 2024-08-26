6aebf069d8e4cf2691510855dd9f3a51d53055e2
Joris Aerts <3611758+JorisAerts@users.noreply.github.com>
2024-08-26 11:17:14 +0200

    [ui] App Dark Mode

--- packages/client/src/main.ts
+++ packages/client/src/main.ts
@@ -3,22 +3,18 @@ import 'js-proxy-ui/components/Transition'
 
 import { app } from './app'
 import { useSystemDarkMode } from 'js-proxy-ui'
-import { watch } from 'vue'
+import { watchEffect } from 'vue'
 
 export const container = document.body.appendChild(document.createElement('div'))
 container.id = 'app'
 
-watch(
-  useSystemDarkMode().isDark,
-  (isDark) => {
-    const html = document.querySelector('html') as HTMLElement
-    if (isDark && !html.classList.contains('dark')) {
-      html.classList.add('dark')
-    } else {
-      html.classList.remove('dark')
-    }
-  },
-  { immediate: true }
-)
+watchEffect(() => {
+  const html = document.querySelector('html') as HTMLElement
+  if (useSystemDarkMode().isDark && !html.classList.contains('dark')) {
+    html.classList.add('dark')
+  } else {
+    html.classList.remove('dark')
+  }
+})
 
 app.mount(container)
