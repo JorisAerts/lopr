3c38a6f504eb7ce54eca5fdc018828b9c7fa9413
Joris Aerts <3611758+JorisAerts@users.noreply.github.com>
2024-08-26 15:10:31 +0200

    [ui] dark mode

--- packages/client/src/composables/dark-mode.ts
+++ packages/client/src/composables/dark-mode.ts
@@ -7,8 +7,6 @@ const HTML_DARK_MODE_CLASS = 'dark-mode'
 export const useDarkMode = () => {
   const isDark = bindLocalStorage('dark-mode')
 
-  console.log({ isDark: isDark.value })
-
   if (isDark.value === undefined) isDark.value = useSystemDarkMode().isDark.value
 
   watchEffect(() => {
@@ -16,9 +14,9 @@ export const useDarkMode = () => {
     const hasDarkClass = html.classList.contains(HTML_DARK_MODE_CLASS)
 
     if (isDark.value) {
-      !hasDarkClass && html.classList.add(HTML_DARK_MODE_CLASS)
+      if (!hasDarkClass) html.classList.add(HTML_DARK_MODE_CLASS)
     } else {
-      hasDarkClass && html.classList.remove(HTML_DARK_MODE_CLASS)
+      if (hasDarkClass) html.classList.remove(HTML_DARK_MODE_CLASS)
     }
   })
 
