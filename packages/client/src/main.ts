554e9e6798b60c773dfad3ec35616cd95c58262b
Joris Aerts <3611758+JorisAerts@users.noreply.github.com>
2024-08-26 11:18:08 +0200

    [ui] no transitions on html-element

--- packages/client/src/main.ts
+++ packages/client/src/main.ts
@@ -2,19 +2,11 @@ import 'js-proxy-ui/style'
 import 'js-proxy-ui/components/Transition'
 
 import { app } from './app'
-import { useSystemDarkMode } from 'js-proxy-ui'
-import { watchEffect } from 'vue'
+import { useHtmlDarkMode } from 'js-proxy-ui'
+
+useHtmlDarkMode()
 
 export const container = document.body.appendChild(document.createElement('div'))
 container.id = 'app'
-
-watchEffect(() => {
-  const html = document.querySelector('html') as HTMLElement
-  if (useSystemDarkMode().isDark && !html.classList.contains('dark')) {
-    html.classList.add('dark')
-  } else {
-    html.classList.remove('dark')
-  }
-})
 
 app.mount(container)
