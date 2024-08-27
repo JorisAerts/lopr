ce94bd769b2bd8031da69e49f114895e42f10941
Joris Aerts <3611758+JorisAerts@users.noreply.github.com>
2024-08-27 20:13:01 +0200

    [client] Break point dialog

--- packages/client/src/components/Breakpoints/VBreakpoints.tsx
+++ packages/client/src/components/Breakpoints/VBreakpoints.tsx
@@ -16,15 +16,30 @@ export const VBreakpoints = defineComponent({
     }
 
     return () => (
-      <VSheet style={{ 'min-width': 'calc(100vw / 3)' }}>
-        <VUrlFilter v-model={model.value} />
-        <VSheet class={['d-flex', 'gap-4', 'align-items-center']}>
-          <VCheckbox v-model={req.value}>Request</VCheckbox>
-          <VCheckbox v-model={res.value}>Response</VCheckbox>
+      <VSheet class={['d-flex', 'gap-2']} style={{ 'min-width': 'calc(100vw / 3)' }}>
+        <VSheet class={['bordered', 'px-2']} style={{ 'min-width': '100px' }}>
+          <ul>
+            <li>
+              <VCheckbox>1</VCheckbox>
+            </li>
+            <li>
+              <VCheckbox>2</VCheckbox>
+            </li>
+            <li>
+              <VCheckbox>3</VCheckbox>
+            </li>
+          </ul>
         </VSheet>
-        <VDialogCardButtons>
-          <VBtn onClick={handleClick}>OK</VBtn>
-        </VDialogCardButtons>
+        <VSheet class={['flex-grow-1']}>
+          <VUrlFilter v-model={model.value} />
+          <VSheet class={['d-flex', 'gap-4', 'align-items-center']}>
+            <VCheckbox v-model={req.value}>Request</VCheckbox>
+            <VCheckbox v-model={res.value}>Response</VCheckbox>
+          </VSheet>
+          <VDialogCardButtons>
+            <VBtn onClick={handleClick}>OK</VBtn>
+          </VDialogCardButtons>
+        </VSheet>
       </VSheet>
     )
   },
