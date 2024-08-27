83ae26ab7029b0293e1e1877a0f202c901c44498
Joris Aerts <3611758+JorisAerts@users.noreply.github.com>
2024-08-27 15:49:30 +0200

    [client] Break points

--- packages/client/src/components/Breakpoints/VBreakpoints.tsx
+++ packages/client/src/components/Breakpoints/VBreakpoints.tsx
@@ -7,6 +7,9 @@ export const VBreakpoints = defineComponent({
 
   setup(props, { slots }) {
     const model = ref()
+    const req = ref(false)
+    const res = ref(false)
+
     const handleClick = (e: MouseEvent) => {
       e.preventDefault()
       console.log({ model: { ...model.value } })
@@ -15,9 +18,9 @@ export const VBreakpoints = defineComponent({
     return () => (
       <VSheet style={{ 'min-width': 'calc(100vw / 3)' }}>
         <VUrlFilter v-model={model.value} />
-        <VSheet class={['d-flex', 'gap-4']}>
-          <VCheckbox>Request</VCheckbox>
-          <VCheckbox>Response</VCheckbox>
+        <VSheet class={['d-flex', 'gap-4', 'align-items-center']}>
+          <VCheckbox v-model={req.value}>Request</VCheckbox>
+          <VCheckbox v-model={res.value}>Response</VCheckbox>
         </VSheet>
         <VDialogCardButtons>
           <VBtn onClick={handleClick}>OK</VBtn>
