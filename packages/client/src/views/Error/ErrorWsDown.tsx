e04e72870e0dbcb10d941bd4ac560c3ee85f2b95
Joris Aerts <3611758+JorisAerts@users.noreply.github.com>
2024-08-29 00:58:01 +0200

    [client] Styling: Web Socket down error page

--- packages/client/src/views/Error/ErrorWsDown.tsx
+++ packages/client/src/views/Error/ErrorWsDown.tsx
@@ -1,6 +1,6 @@
 import type { PropType } from 'vue'
 import { defineComponent } from 'vue'
-import { VContainer, VIcon } from 'js-proxy-ui'
+import { VBtn, VContainer, VIcon, VSheet } from 'js-proxy-ui'
 import { useRouter } from 'vue-router'
 
 export const ErrorWsDown = defineComponent({
@@ -15,12 +15,14 @@ export const ErrorWsDown = defineComponent({
   setup() {
     const router = useRouter()
     return () => (
-      <VContainer vertical center class={['fill-height', 'gap-2', 'align-items-center']}>
-        <VIcon name={'SentimentVeryDissatisfied'} size={96} />
-        <span class={['align-center', 'mt-4']}>Darn, the Web Socket seems to be down.</span>
-        <a href={'javascript:history.back()'} onClick={router.back}>
-          Back
-        </a>
+      <VContainer vertical center class={['fill-height', 'gap-2', 'align-items-center', 'justify-center']}>
+        <VSheet class={['d-flex', 'flex-column', 'align-items-center']}>
+          <VIcon name={'SentimentVeryDissatisfied'} size={128} />
+          <span class={['align-center', 'my-4']}>Darn, the Web Socket seems to be down.</span>
+          <VBtn transparent onClick={router.back}>
+            ← Back
+          </VBtn>
+        </VSheet>
       </VContainer>
     )
   },
