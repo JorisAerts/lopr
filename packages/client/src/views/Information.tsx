576dd907bf99349b34ed6cfb6c6cacecceebd662
Joris Aerts <3611758+JorisAerts@users.noreply.github.com>
2024-08-28 10:31:50 +0200

    [client] display distinct certificates

--- packages/client/src/views/Information.tsx
+++ packages/client/src/views/Information.tsx
@@ -1,4 +1,4 @@
-import type { CSSProperties} from 'vue';
+import type { CSSProperties } from 'vue'
 import { computed, defineComponent, TransitionGroup } from 'vue'
 import { VCard, VIcon, VSheet, VTooltip } from 'js-proxy-ui'
 import { useCertificateStore } from '../stores/certificates'
@@ -8,7 +8,7 @@ export const Information = defineComponent({
   setup() {
     const certStore = useCertificateStore()
     const certificates = computed(() =>
-      certStore.certificates?.toSorted().map((certFile) => {
+      [...certStore.certificates].toSorted().map((certFile) => {
         const file = `file://${certFile.replace(/\\/, '/')}`
         const cert = file.substring(file.lastIndexOf('/') + 1, file.lastIndexOf('.'))
         return { cert, file }
@@ -22,10 +22,10 @@ export const Information = defineComponent({
           <h3 class={['mt-6']}>Certificates</h3>
           Below is a list of generated certificated which will be spoofed into HTTPS calls.
           <div class={['d-flex', 'mt-4', 'gap-2', 'flex-wrap']}>
-            {certStore.certificates.length ? (
+            {!certStore.isEmpty ? (
               <TransitionGroup>
                 {certificates.value.map(({ file, cert }) => (
-                  <VCard class={['pa-2', 'd-flex', 'align-items-center']} key={file} style={{ width: 'calc(20% - 8px)' }}>
+                  <VCard class={['pa-2', 'd-flex', 'align-items-center', 'overflow-ellipsis']} key={file} style={{ width: 'calc(20% - 8px)' }}>
                     <VIcon name={'ShieldLock'} class={'mr-2'} size={33} style={{ float: 'left' }} />
                     <VTooltip text={cert}>
                       <a href={file} target={'cert'}>
