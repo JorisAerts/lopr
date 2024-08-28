33fc805c3bc17fe4b95c22a7bc5e30451791cc84
Joris Aerts <3611758+JorisAerts@users.noreply.github.com>
2024-08-29 00:43:11 +0200

    [ui] CSS: header margins

--- packages/client/src/components/AppPreferences/AppPreferences.tsx
+++ packages/client/src/components/AppPreferences/AppPreferences.tsx
@@ -11,7 +11,7 @@ export const AppPreferences = defineComponent({
       <VCard class={['fill-height', 'overflow-auto', 'flex-grow-1', 'd-flex', 'flex-column', 'pa-3', 'gap-2']}>
         <h2 class={'mb-2'}>Preferences</h2>
         <VCard class={['pa-3']}>
-          <h3 class={'mb-2'}>Appearance</h3>
+          <h3>Appearance</h3>
           <VLabel class={['d-flex', 'flex-row']} style={{ '--input-label-font-weight': 'initial' }}>
             <span class={['pr-1']} style={{ margin: 'auto 0' }}>
               Dark Mode:
@@ -20,7 +20,7 @@ export const AppPreferences = defineComponent({
           </VLabel>
         </VCard>
         <VCard class={['pa-3']}>
-          <h3 class={'mb-2'}>Reverse Proxy</h3>
+          <h3>Reverse Proxy</h3>
           <VTooltip text={'SSL Certificates will be spoofed, \nso that the transmitted data can be inspected.'}>
             <VCheckbox label={'Enable SSL Proxying'} v-model={preferencesStore.proxySSL} />
           </VTooltip>
