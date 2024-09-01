5730786ddb3f1394d98e542f2579805d24f65d72
Joris Aerts <3611758+JorisAerts@users.noreply.github.com>
2024-09-01 13:49:50 +0200

    [ui] Pie Chart: styling

--- packages/ui/src/components/Charts/VPieChart.tsx
+++ packages/ui/src/components/Charts/VPieChart.tsx
@@ -13,7 +13,7 @@ export const VPieChart = defineComponent({
 
   props: {
     values: { type: Array as PropType<{ value: number; color?: string }[]>, default: () => [] },
-    borderWidth: { type: Number, default: 0.5 },
+    borderWidth: { type: Number, default: 1 },
     colors: {
       type: Array,
       default: () => [
@@ -28,7 +28,7 @@ export const VPieChart = defineComponent({
     },
   },
 
-  setup(props, { slots }) {
+  setup(props) {
     const svg = ref()
     const total = computed(() => props.values.reduce((a, { value }) => a + value, 0))
     const width = ref(1)
@@ -42,19 +42,19 @@ export const VPieChart = defineComponent({
 
     return () => {
       let current = 0
-      const w = width.value - 2 * props.borderWidth
-      const h = height.value - 2 * props.borderWidth
+      const w = width.value - props.borderWidth
+      const h = height.value - props.borderWidth
       return (
-        <svg class={'v-pie-chart'} viewBox={`${width.value / -2 - props.borderWidth} ${height.value / -2 - props.borderWidth} ${width.value} ${height.value}`} ref={svg}>
+        <svg class={'v-pie-chart'} viewBox={`${width.value / -2} ${height.value / -2} ${width.value} ${height.value}`} ref={svg}>
           {props.values.map((item, index) => {
             const { value } = item
             const percent = value / total.value
             const [startX, startY] = getCoordinatesForPercent(current)
             const [endX, endY] = getCoordinatesForPercent((current += percent))
             const d = `M ${(startX * w) / 2} ${(startY * h) / 2} A  ${w / 2} ${h / 2} 0 ${percent > 0.5 ? 1 : 0} 1 ${(endX * w) / 2} ${(endY * h) / 2} L 0 0 Z`
-            const strokeWidth = props.borderWidth != 1 ? props.borderWidth : undefined
             const fill = item.color ?? (props.colors[index % props.colors.length] as string)
-            return <path d={d} fill={fill} stroke-width={strokeWidth} />
+            const style = props.borderWidth != 1 ? { 'stroke-width': props.borderWidth } : undefined
+            return <path d={d} fill={fill} style={style} />
           })}
         </svg>
       )
