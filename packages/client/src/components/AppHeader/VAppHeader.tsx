4ae6f754e85ac7e5fbf4e0529c5583047bb6dd1a
Joris Aerts <3611758+JorisAerts@users.noreply.github.com>
2024-09-02 13:01:30 +0200

    [client] favicon dynamic color

--- packages/client/src/components/AppHeader/VAppHeader.tsx
+++ packages/client/src/components/AppHeader/VAppHeader.tsx
@@ -1,13 +1,15 @@
 import { computed, defineComponent, Teleport } from 'vue'
 import { APP_NAME } from 'lopr-shared'
 import { DominoMask_Fill } from 'lopr-ui/icons'
+import { useSystemDarkMode } from 'lopr-ui'
 
 export const VAppHeader = defineComponent({
   name: 'v-app-header',
 
   setup() {
+    const { isDark } = useSystemDarkMode()
     const iconSize = 512
-    const iconColor = computed(() => 'white')
+    const iconColor = computed(() => (isDark.value ? 'white' : 'black'))
     const favicon = computed(() =>
       btoa(`<svg width="${iconSize}" height="${iconSize}" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" style="fill: ${iconColor.value}">
           <path d="${DominoMask_Fill}" />
