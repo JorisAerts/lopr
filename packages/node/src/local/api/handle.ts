decfd8d769087fd928d1e6c2a8a63a2168688cb2
Joris Aerts <3611758+JorisAerts@users.noreply.github.com>
2024-09-13 20:47:42 +0200

    [node] API handlers

--- packages/node/src/local/api/handle.ts
+++ packages/node/src/local/api/handle.ts
@@ -1,6 +1,6 @@
 import * as handleDataUUID from './handle-data-uuid'
 import * as handleDataCert from './handle-data-uuid'
-import * as handleDataServerInfo from './handle-server-info'
+import * as handleServerInfo from './handle-server-info'
 import * as handleDataState from './handle-state'
 import * as handlePreferences from './handle-prefs'
 import type { ProxyRequest } from '../../server/ProxyRequest'
@@ -8,11 +8,6 @@ import type { ProxyResponse } from '../../server/ProxyResponse'
 import type { UrlWithParsedQuery } from 'url'
 import type { InternalProxyState } from '../../server/server-state'
 
-const handlers = [handleDataUUID, handleDataCert, handleDataServerInfo, handleDataState, handlePreferences]
+const handlers = [handleDataUUID, handleDataCert, handleServerInfo, handleDataState, handlePreferences]
 
-export const handle = (url: UrlWithParsedQuery, req: ProxyRequest, res: ProxyResponse, state: InternalProxyState) => {
-  for (const handler of handlers) {
-    if (handler.handle(url, req, res, state)) return true
-  }
-  return false
-}
+export const handle = (url: UrlWithParsedQuery, req: ProxyRequest, res: ProxyResponse, state: InternalProxyState) => handlers.some(({ handle }) => handle(url, req, res, state))
